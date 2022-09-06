d329e72fe17dc84ecee2d7f584bb143a197e68b4
detayaa <drugochenko.taya@mail.ru>
2022-09-06 20:41:19 +0700

    3

--- src/main.java
+++ src/main.java
@@ -1,12 +1,18 @@
 public class main {
     public static void main(String[] args) {
-        //задание 1
+        System.out.println("задание 1");
         for (int i = 1; i <= 10; i++) {
             System.out.println(i);
         }
-        // задание 2
+        System.out.println("задание 2");
         for (int i = 10; i >= 1; i--) {
             System.out.println(i);
+        }
+        System.out.println("задание 3");
+        for (int i = 0; i <= 17; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
         }
     }
 }
